d2a0f3a78e47bd86e99626cb6eb9afa712f84a1e
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-28 08:33:10 +0300

    refactor: Deal With Number Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,17 @@ package org.example
 
 fun main() {
     println("Hello world!")
+    val byteNum:Byte = 50
+    val shortNum:Short = 130
+    val intNum:Int = 35000
+    val longNum:Long = 25446321321321
+    println(byteNum)
+    println(shortNum)
+    println(intNum)
+    println(longNum)
+
+    val weight:Float = 5.5f
+    val height:Double = 172.68
+    println(weight)
+    println(height)
 }
