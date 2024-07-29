718d59ea7083c774dc3d4d77e5f1159e2c16cd48
Ali Mansour <dev.ali.mansour@gmail.com>
2024-07-29 06:11:39 +0300

    refactor: Deal With Boolean And Char Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,31 @@ package org.example
 
 fun main() {
     println("Hello world!")
-    val byteNum:Byte = 50
-    val shortNum:Short = 130
-    val intNum:Int = 35000
-    val longNum:Long = 25446321321321
+    val byteNum: Byte = 50
+    val shortNum: Short = 130
+    val intNum: Int = 35000
+    val longNum: Long = 25446321321321
     println(byteNum)
     println(shortNum)
     println(intNum)
     println(longNum)
 
-    val weight:Float = 5.5f
-    val height:Double = 172.68
+    val weight: Float = 5.5f
+    val height: Double = 172.68
+    val weight2: Float = 5.4E6f
+    val height2: Double = 172.68e3
     println(weight)
     println(height)
+    println(weight2)
+    println(height2)
+
+    val isSucceed: Boolean = true
+    val isMonday = false
+    println(isSucceed)
+    println(isMonday)
+
+    val grade: Char = 'A'
+    val gender = 'm'
+    println(gender)
+    println(grade)
 }
