917ef26802009a39aedb3ec1be21abbbe80dfe75
Ali Mansour <dev.ali.mansour@gmail.com>
2024-08-02 14:46:55 +0300

    refactor: Create Comments.kt File To Teach Kotlin Comments.

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,56 @@
+package org.example
+
+fun main() {
+
+    /*
+    In this lesson We learn how to declare variables
+    Integer variables
+    Fraction variables
+    Boolean variables
+    Char variables
+     */
+
+    // Integer Variable Declaration
+    val byteNum: Byte = 50
+    val shortNum: Short = 130
+    val intNum: Int = 35000
+    val longNum: Long = 25446321321321
+
+    println(byteNum)
+    println(shortNum)
+    println(intNum)
+    println(longNum)
+
+    // Fractional Variables Declaration
+    val weight: Float = 5.5f
+    val height: Double = 172.68
+    val weight2: Float = 5.4E6f
+    val height2: Double = 172.68e3
+    println(weight)
+    println(height)
+    println(weight2)
+    println(height2)
+
+    // Boolean variables Declaration
+    val isSucceed: Boolean = true
+    val isMonday = false
+    println(isSucceed)
+    println(isMonday)
+
+    // Character Variables Declaration
+    val grade: Char = 'A'
+    val gender = 'm'
+    println(gender)
+    println(grade)
+
+    // ToDo Validate student degree to detect grade
+
+    // Username
+    val str1 = "admin"
+    // Password
+    val str2 = "123456"
+
+    val username = "admin"
+    val password= "123456"
+
+}
