efab871c82267577948434f43f2a781f198a6286
Ali Mansour <dev.ali.mansour@gmail.com>
2024-08-14 08:35:27 +0300

    refactor: Create WhileLoop.kt To Teach How To Iterate Your Code Using While Loop .

---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main() {
+    /*var count = 1
+    while (count <5){
+        println(count++)
+//        count++
+    }*/
+
+    val secretNumber = (1..5).random()
+    var guess = readln().toInt()
+    while (guess!=secretNumber){
+        println("Try again!")
+        guess = readln().toInt()
+    }
+}
