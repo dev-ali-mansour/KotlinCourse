4c7eaf815f2f7636ca327842a0c75ee32496ff96
Ali Mansour <dev.ali.mansour@gmail.com>
2024-08-24 07:27:51 +0300

    refactor: Create ExceptionHandling.kt To Teach How To Handle Exceptions In Kotlin.

---
+++ src/main/kotlin/ExceptionHandling.kt
@@ -0,0 +1,33 @@
+package org.example
+
+import java.io.IOException
+
+fun main() {
+     try{
+         val result = riskyOperation()
+     }catch (e:Exception){
+         println("An error occurred ${e.message}")
+     }
+
+    try {
+        val data = fetchDataFromServer()
+    } catch (e: IOException) {
+        println("An error occurred: ${e.message}")
+    } finally {
+        closeConnection()
+    }
+}
+
+fun closeConnection() {
+    println("Close connection")
+}
+
+fun fetchDataFromServer(): String {
+    throw IOException("Server unreachable")
+    return "Server response"
+}
+
+fun riskyOperation() {
+    println("Risky operation")
+    throw Exception("Invalid User data!")
+}
