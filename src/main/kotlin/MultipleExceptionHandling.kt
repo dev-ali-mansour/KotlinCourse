dbb3dd1d7084b727195767c783e22588daa71abe
Ali Mansour <dev.ali.mansour@gmail.com>
2024-08-25 08:03:19 +0300

    refactor: Create MultipleExceptionHandling.kt To Teach How To Handle Multiple Exceptions In Kotlin.

---
+++ src/main/kotlin/MultipleExceptionHandling.kt
@@ -0,0 +1,42 @@
+package org.example
+
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.text.ParseException
+
+fun main() {
+    try {
+        readFile("data.json")
+    } catch (e: IOException) {
+        // Handle IOException
+    } catch (e: FileNotFoundException) {
+        // Handle FileNotFoundException
+    } catch (e: ParseException) {
+        // Handle ParseException
+    } catch (e: Exception) {
+        // Handle any other exception
+    }
+
+    try {
+        readFile("data.json")
+    } catch (e: Exception) {
+        when (e) {
+            is FileNotFoundException->{/* Handle FileNotFoundException */}
+            is IOException -> {/* Handle IOException */}
+            is ParseException-> { /* Handle ParseException */ }
+            else -> {/* Handle any other exception */}
+        }
+    }
+
+    runCatching { readFile("data.json") }
+        .onFailure { e->
+            when (e) {
+                is FileNotFoundException->{/* Handle FileNotFoundException */}
+                is IOException -> {/* Handle IOException */}
+                is ParseException-> { /* Handle ParseException */ }
+                else -> {/* Handle any other exception */}
+            }
+        }
+}
+
+fun readFile(path: String) {}
