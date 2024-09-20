a9a9bd0b0f105c4ccf102e22efb13484537ff561
Ali Mansour <dev.ali.mansour@gmail.com>
2024-09-20 11:43:40 +0300

    refactor: Create Polymorphism.kt To Teach Polymorphism Principle In Kotlin.

---
+++ src/main/kotlin/Polymorphism.kt
@@ -0,0 +1,44 @@
+package org.example
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    fun add(a: Int, b: Int, c: Int): Int {
+//        return a + b + c
+        return add(a, add(b, c))
+    }
+
+    fun add(a: Double, b: Double): Double {
+        return a + b;
+    }
+}
+
+open class Vehicle {
+    open val brand = "Unkonwn"
+
+    open fun drive() {
+        println("The vehicle is moving")
+    }
+}
+
+class Car : Vehicle() {
+    override val brand: String = "Toyota"
+
+    override fun drive() {
+        super.drive()
+        println("The car is moving smoothly")
+    }
+}
+
+fun main() {
+//    val calc = Calculator()
+//    println(calc.add(1, 3))
+//    println(calc.add(2, 6, 3))
+//    println(calc.add(3.2, 4.6))
+
+    val car = Car()
+    println("Car brand: ${car.brand}")
+    car.drive()
+}
