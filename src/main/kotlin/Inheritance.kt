05ada2498dc280a1da47ce62bf0a0ef85bc8accd
Ali Mansour <dev.ali.mansour@gmail.com>
2024-09-20 11:02:09 +0300

    refactor: Create Inheritance.kt To Teach Inheritance Principle In Kotlin.

---
+++ src/main/kotlin/Inheritance.kt
@@ -0,0 +1,31 @@
+package org.example
+
+open class Animal{
+    var name: String = "Unknown"
+
+    fun eat(){
+        println("$name is eating")
+    }
+
+    open fun sound(){
+        println("Animal is making a sound")
+    }
+}
+
+class Dog:Animal(){
+    fun bark(){
+        println("Dog is barking")
+    }
+
+    override fun sound(){
+        println("Dog is making a sound")
+    }
+}
+
+fun main() {
+val myDog = Dog()
+    myDog.name = "Jack"
+    myDog.sound()
+    myDog.bark()
+    myDog.eat()
+}
