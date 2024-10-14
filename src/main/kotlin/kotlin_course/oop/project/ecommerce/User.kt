f97239c6c7aedec395d6325796bb2f91e1a88ced
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-14 04:35:32 +0300

    refactor: Create User Data Class In E-Commerce System.

---
+++ src/main/kotlin/kotlin_course/oop/project/ecommerce/User.kt
@@ -0,0 +1,10 @@
+package org.example.kotlin_course.oop.project.ecommerce
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val age: Short,
+    val email: String,
+    val username: String,
+    val password: String
+)
