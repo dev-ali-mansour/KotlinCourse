90428e57dea00decff65bd2563fe2ae49b6f07cf
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-14 04:39:24 +0300

    refactor: Create System.kt File Contains The Main Function As Application Start Point Of E-Commerce System.

---
+++ src/main/kotlin/kotlin_course/oop/project/ecommerce/System.kt
@@ -0,0 +1,10 @@
+package org.example.kotlin_course.oop.project.ecommerce
+
+fun main() {
+    println("######################")
+    println("E-Commerce System")
+    println("######################")
+    showAuthMenu()
+}
+
+
