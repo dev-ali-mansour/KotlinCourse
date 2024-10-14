d31e52ddc6327cc78373ce733d94f71e79b41cd5
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-14 04:37:51 +0300

    refactor: Create MainScreen.kt File In E-Commerce System.

---
+++ src/main/kotlin/kotlin_course/oop/project/ecommerce/MainScreen.kt
@@ -0,0 +1,68 @@
+package org.example.kotlin_course.oop.project.ecommerce
+
+import kotlin.system.exitProcess
+
+fun showMainScreen() {
+    val userManager = UserManager
+    if (userManager.isAuthenticated()) showMainMenu(userManager)
+    else showAuthMenu()
+}
+
+fun showMainMenu(userManager: UserManager) {
+    println("###########")
+    println("Main Screen")
+    println("###########")
+    println("Welcome back ${userManager.getFullName()}")
+    println("Choose an action:")
+    println("1) Show products")
+    println("2) Logout")
+    var repeat = true
+    while (repeat) {
+        val choice = readln().toInt()
+        when (choice) {
+            1 -> {
+                showProducts()
+                repeat = false
+            }
+
+            2 -> {
+                userManager.logout()
+                showMainScreen()
+                repeat = false
+            }
+
+            else -> println("Invalid choice! try again")
+        }
+    }
+}
+
+fun showAuthMenu() {
+    var repeat = true
+    while (repeat) {
+        runCatching {
+            println("Choose an action")
+            println("1) Sign up")
+            println("2) Login")
+            println("3) Exit")
+            val choice = readln().toInt()
+            when (choice) {
+                1 -> {
+                    register()
+                    repeat = false
+                }
+
+                2 -> {
+                    login()
+                    repeat = false
+                }
+
+                3 -> {
+                    println("System Exit. Bye!")
+                    exitProcess(0)
+                }
+
+                else -> println("Invalid choice! try again")
+            }
+        }.onFailure { error -> println("Invalid Choice! ${error.message}") }
+    }
+}
