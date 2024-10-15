a202978c3a500ac86734b6e0dc7a880258a7103e
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-15 06:41:09 +0300

    refactor: Create CartImpl Class To Implement Cart Interface In E-Commerce System.

---
+++ src/main/kotlin/kotlin_course/oop/project/ecommerce/CartImpl.kt
@@ -0,0 +1,26 @@
+package org.example.kotlin_course.oop.project.ecommerce
+
+val products = listOf(
+    Product(id = 1, name = "Laptop", price = 15000.0),
+    Product(id = 2, name = "Mouse", price = 90.0),
+    Product(id = 3, name = "Mouse Pad", price = 30.0),
+    Product(id = 4, name = "Speakers", price = 650.0),
+    Product(id = 5, name = "Laptop Bag", price = 450.0)
+)
+
+class CartImpl : Cart {
+
+    private val _items = mutableListOf<Product>()
+
+    override val items: List<Product> = _items
+
+    override fun addProduct(productId: Int) {
+        val product = products.firstOrNull { it.id == productId }
+        product?.let {
+            _items.add(it)
+            println("${it.name} added to cart.")
+        } ?: println("Invalid product Id!")
+    }
+
+    override fun getTotalPrice(): Double = _items.sumOf { it.price }
+}
