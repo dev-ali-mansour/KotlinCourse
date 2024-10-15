0b33ceb051f20490767edbd266adbaf1a2a3affc
Ali Mansour <dev.ali.mansour@gmail.com>
2024-10-15 06:40:04 +0300

    refactor: Create OrderImpl Class To Implement Order Interface In E-Commerce System.

---
+++ src/main/kotlin/kotlin_course/oop/project/ecommerce/OrderImpl.kt
@@ -0,0 +1,17 @@
+package org.example.kotlin_course.oop.project.ecommerce
+
+class OrderImpl(private val cart: Cart):Order {
+
+    override fun placeOrder() {
+        val total = cart.getTotalPrice()
+        println("Order placed")
+        cart.items.forEach { println(it) }
+        println("Total amount: $total")
+    }
+
+    override fun pay(amount: Double, method: PaymentMethod) {
+        if (amount != cart.getTotalPrice())
+            throw Throwable(message = "Invalid amount!")
+        println("Order has been paid successfully!")
+    }
+}
